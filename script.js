1e11a06e5d50deecc5a154e85efec0931fff6298
LnX <luan38.eric@gmail.com>
2022-02-16 14:03:28 -0300

    Contador feito com Redux no JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const lingua = document.getElementById('lingua');
+
+function ativaLingua(){
+    alert('Olá, eu sou o BobsConta. Minha lógica é utilizando o Redux.')
+}
+
+lingua.addEventListener('click', ativaLingua);
